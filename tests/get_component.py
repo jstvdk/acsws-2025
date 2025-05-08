1b4b5e3055df1415715e5dab5046bd96afc584fc
Vadym Voitsekhovskyi <jstvdk@gmail.com>
2025-05-08 16:24:50 +0200

    New tet

--- tests/get_component.py
+++ tests/get_component.py
@@ -1,6 +1,7 @@
 from Acspy.Clients.SimpleClient import PySimpleClient
+import TYPES
 
 cli = PySimpleClient()
 comp = cli.getComponent("ACS_astroDatabase")
 
-print(comp.storeProposal([4]))
+print('The Component was retrieved succesfuly and proposal status is : {comp.getProposalStatus(1)}')
